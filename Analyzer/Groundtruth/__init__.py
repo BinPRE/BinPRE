5ce312c7778f88c9945b8c29df2b6c6cd87b3c7b
BinPRE <982978624@qq.com>
2024-08-04 11:50:35 +0800

    Update __init__.py (add more Groundtruth)

--- Analyzer/Groundtruth/__init__.py
+++ Analyzer/Groundtruth/__init__.py
@@ -3,3 +3,46 @@ from .modbus import modbus_Semantic_Groundtruth
 from .modbus import modbus_commandOffset
 from .modbus import modbus_lengthOffset
 from .modbus import modbus_Semantic_Functions_Groundtruth
+
+from .s7 import s7_Syntax_Groundtruth
+from .s7 import s7_Semantic_Groundtruth
+from .s7 import s7_commandOffset
+from .s7 import s7_lengthOffset
+from .s7 import s7_Semantic_Functions_Groundtruth
+
+
+from .dnp3 import dnp3_Syntax_Groundtruth
+from .dnp3 import dnp3_Semantic_Groundtruth
+from .dnp3 import dnp3_commandOffset
+from .dnp3 import dnp3_lengthOffset
+from .dnp3 import dnp3_Semantic_Functions_Groundtruth
+
+from .eip import eip_Syntax_Groundtruth
+from .eip import eip_Semantic_Groundtruth
+from .eip import eip_commandOffset
+from .eip import eip_lengthOffset
+from .eip import eip_Semantic_Functions_Groundtruth
+
+from .dns import dns_Syntax_Groundtruth
+from .dns import dns_Semantic_Groundtruth
+from .dns import dns_commandOffset
+from .dns import dns_lengthOffset
+from .dns import dns_Semantic_Functions_Groundtruth
+
+from .ftp import ftp_Syntax_Groundtruth
+from .ftp import ftp_Semantic_Groundtruth
+from .ftp import ftp_commandOffset
+from .ftp import ftp_lengthOffset
+from .ftp import ftp_Semantic_Functions_Groundtruth
+
+from .tftp import tftp_Syntax_Groundtruth
+from .tftp import tftp_Semantic_Groundtruth
+from .tftp import tftp_commandOffset
+from .tftp import tftp_lengthOffset
+from .tftp import tftp_Semantic_Functions_Groundtruth
+
+from .http import http_Syntax_Groundtruth
+from .http import http_Semantic_Groundtruth
+from .http import http_commandOffset
+from .http import http_lengthOffset
+from .http import http_Semantic_Functions_Groundtruth
